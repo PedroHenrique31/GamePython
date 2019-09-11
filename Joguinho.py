27621ecf17b9e6e94db2eab33201ec8d600343a6
PedroHenrique31 <pedrohcaraujo1@hotmail.com>
2019-09-11 15:44:26 -0300

    Criando o projeto inicial e testando o uso paralelo com git

---
+++ Joguinho.py
@@ -0,0 +1,11 @@
+'''
+                Projeto de jogo de advinhação
+    Como parte do curso de pyhton da alura cursos online tenho feito esse jogo de advinhação
+é um conceito simples mas bastante escalável.
+'''
+print("####################################################################")
+print("################### Jogo de advinhação #############################")
+print("####################################################################")
+
+# Variaveis de jogo
+numero_secreto=42
