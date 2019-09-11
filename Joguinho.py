9235daa58b72c7633393e6bf4dbe801b440d9176
Pedro Henrique <pedrohcaraujo1@hotmail.com>
2019-09-11 16:05:57 -0300

    Copiando versão atualizada para treinar laços de repetição e condicionais;agora ele faz uma melhor de 5

--- Joguinho.py
+++ Joguinho.py
@@ -9,3 +9,26 @@ print("####################################################################")
 
 # Variaveis de jogo
 numero_secreto=42
+total_tentativas=5
+rodada=1
+
+while(rodada<=total_tentativas):
+    print("Tentativa {} de {}",rodada,total_tentativas)
+    chute_str=input("Dá seu chute ae\n")
+    print("Você está certo de que é ",chute_str,"?\n")
+    chute=int(chute_str)
+
+    acertou=(chute==numero_secreto)
+    maior=(chute>numero_secreto)
+    menor=(chute<numero_secreto)
+
+    if(acertou):
+        print("Ae mizeravi\n")
+    else:
+        if(maior):
+            print("Você errou! foi muito longe\n")
+        elif(menor):
+            print("Você errou! muito baixo\n")
+
+    rodada=rodada+1
+
