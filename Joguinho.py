a9b30ede880fa58956263047bbcbfaa1b931723c
Pedro Henrique <pedrohcaraujo1@hotmail.com>
2019-09-11 17:37:51 -0300

    Nova versão com formatação e testes atualizados, além de realizar testes para números validos e tratar excessões

--- Joguinho.py
+++ Joguinho.py
@@ -12,14 +12,18 @@ numero_secreto=42
 total_tentativas=2
 rodada=1
 
-while(rodada<=total_tentativas):
+for rodada in range(1,total_tentativas+1):
     rodada_str=str(rodada)
     total_tentativas_str=str(total_tentativas)
     #não entendi porque tem que ser .format() mas só funcionou assim
     print("===> Tentativa {} de {}".format(rodada,total_tentativas))
-    chute_str=input("Dá seu chute ae\n")
+    chute_str=input("Dá seu chute ae entre 1 e 10 milhões\n")
     #print("Você está certo de que é ",chute_str,"?\n")
     chute=int(chute_str)
+    if((chute<1) or (chute>10000000)):
+        print("ENTRE 1 E 10 MILHÕÕÕES!!\n")
+        continue#esse comando diferentemente do break não sai do laço, ele apenas pula essa iteração
+
 
     ''' Valores booleanos criados diretamente fora, para facilitar a leitura, nunca pensei em fazer
         isso em outra linguagem
@@ -30,6 +34,7 @@ while(rodada<=total_tentativas):
 
     if(acertou):
         print("Ae mizeravi\n")
+        break
     else:
         if(maior):
             print("Você errou! foi muito longe\n")
