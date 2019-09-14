462e88842faa8619b5ca1899db9253c5d7e77163
Pedro Henrique <pedrohcaraujo1@hotmail.com>
2019-09-14 15:47:59 -0300

    Atualização para gerar numeros aleatórios,no caso entre 1 e 100, e também a entrada de um cheet de código para facilitar a depuração.

--- Joguinho.py
+++ Joguinho.py
@@ -3,14 +3,24 @@
     Como parte do curso de pyhton da alura cursos online tenho feito esse jogo de advinhação
 é um conceito simples mas bastante escalável.
 '''
+import random
+
 print("####################################################################")
 print("################### Jogo de advinhação #############################")
 print("####################################################################")
 
 # Variaveis de jogo
-numero_secreto=42
+numero_secreto=round(random.random()*100)#agora gera numeros aleatorios entre 0 e 100
 total_tentativas=2
 rodada=1
+cheet=4096
+#Agora uma pequena função de cheet para testar o jogo
+def resp(codigo):
+    senha=4096
+    if(codigo==senha):
+        print("Numero_secreto= ", numero_secreto)
+    else:
+        print("Numero_secreto= Achou que ia ter respota né... raaa\n")
 
 for rodada in range(1,total_tentativas+1):
     rodada_str=str(rodada)
@@ -20,7 +30,7 @@ for rodada in range(1,total_tentativas+1):
     chute_str=input("Dá seu chute ae entre 1 e 10 milhões\n")
     #print("Você está certo de que é ",chute_str,"?\n")
     chute=int(chute_str)
-    if((chute<1) or (chute>10000000)):
+    if((chute<1) or (chute>100)):
         print("ENTRE 1 E 10 MILHÕÕÕES!!\n")
         continue#esse comando diferentemente do break não sai do laço, ele apenas pula essa iteração
 
@@ -31,12 +41,15 @@ for rodada in range(1,total_tentativas+1):
     acertou=(chute==numero_secreto)
     maior=(chute>numero_secreto)
     menor=(chute<numero_secreto)
-
+    cheetou=(chute==cheet)
+    
     if(acertou):
         print("Ae mizeravi\n")
         break
     else:
-        if(maior):
+        if (cheetou):  # invocação do cheet
+            resp(chute)
+        elif(maior):
             print("Você errou! foi muito longe\n")
         elif(menor):
             print("Você errou! muito baixo\n")
