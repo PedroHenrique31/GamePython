585e6e07f625542b6fc234ab68120f1ae0956366
Pedro Henrique <pedrohcaraujo1@hotmail.com>
2019-09-14 17:01:58 -0300

    Adicionado novo módulo para selecionar entre os jogos.

---
+++ GameSelector.py
@@ -0,0 +1,11 @@
+print("*************************************")
+print("******** OLÁÁÁ!! Temos vários Jogos!\n")
+print("******* Escolha seu jogooo************")
+
+print("(1)Forca (2)Advinhação")
+input("Qual a sua escolha? ")
+
+if(jogo==1):
+    print("Então é forca!")
+else:
+    print("Então vai ser advinhação!!")
