c160e22ef1e5de98e357f3c123f91813f88d5567
Pedro Henrique <pedrohcaraujo1@hotmail.com>
2019-10-08 19:23:20 -0300

    Ultimas atualizacoes para insercao de funcoes e melhorias de apresentacao.
    Signed-off-by: Pedro Henrique <pedrohcaraujo1@hotmail.com>

--- forca.py
+++ forca.py
@@ -3,25 +3,23 @@
     Obs:Para não ser xecutado automaticamente é preciso definir apenas funções no módulo.
 '''
 import random
+
+
+
+
+
 def jogar_forca():
 
     print("***************************************")
     print("***Bem vindo ao jogo da forca**********")
     print("***************************************")
 
-    #Lê de um arquivo as palavras para o jogo
-    with open("palavras.txt","r") as arquivo
-    palavras=[]
+    palavras=carrega_palavras()
 
-    for linha in arquivo:
-        linha=linha.strip()
-        palavras.append(linha)
-    print(palavras)
-
-    palavra_sorteada=random.randrange(0,len(palavras))
+    palavra_sorteada=random.randrange(0,len(palavras))#seleciona uma palavra da lista
     #variáveis de jogo
     palavra_secreta=palavras[palavra_sorteada].upper()
-    tamanho_chances=6
+    tamanho_chances=7 #TODO: tentar melhorar isso para algo dinamico
     letra_certa=["_" for letras in palavra_secreta]
 
     enforcou=False
@@ -31,28 +29,133 @@ def jogar_forca():
     print(letra_certa)  # Imprime a forca pra dar uma previa
     #Game loop
     while(not enforcou and not acertou):
-
-        chute=input("Dá um chute ae: ")
-        chute=chute.strip().upper()#tira os espacos e converte tudo pra maiusculo
-
-        posicao=0
+        #Apresenta a lê os chutes
+        chute=pede_chute()
+        #Avalia se tem essa letra na palavra,senão incrementa erros
         if(chute in palavra_secreta):
-            for letra in palavra_secreta:
-                if(chute==letra):#letra.upper, tranforma udo em caps lock
-                    letra_certa[posicao]=letra
-                posicao = posicao+1
+            avalia_chute(chute,letra_certa,palavra_secreta)
         else:
             erros+=1
-
+            desenha_forca(erros)
+        #define o que é considerado um acerto/erro
         acertou=("_" not in letra_certa)
         enforcou=(erros==tamanho_chances)
         print(letra_certa)
     print("Jogando...")
+    #Avalia se já ganhou ou não para parar o jogo
     if(acertou):
-        print("Parabéns!!! Você venceu!! ;)\n")
+        imprime_vencedor()
     else:
-        print("Você perdeu!!")
+        imprime_perdedor(palavra_secreta)
     print("Fim de Jogo!!!\n")
+###################################### Funções auxiliares de jogo ######################################################
+def pede_chute():
+    chute=input("Dá um chute ae: ")
+    chute = chute.strip().upper()  # tira os espacos e converte tudo pra maiusculo
+    return chute
+
+
+def imprime_vencedor():
+        print("Parabéns, você ganhou!")
+        print("       ___________      ")
+        print("      '._==_==_=_.'     ")
+        print("      .-\\:      /-.    ")
+        print("     | (|:.     |) |    ")
+        print("      '-|:.     |-'     ")
+        print("        \\::.    /      ")
+        print("         '::. .'        ")
+        print("           ) (          ")
+        print("         _.' '._        ")
+        print("        '-------'       ")
+
+
+def imprime_perdedor(palavra):
+        print("Puxa, você foi enforcado!")
+        print("A palavra era {}".format(palavra))
+        print("    _______________         ")
+        print("   /               \       ")
+        print("  /                 \      ")
+        print("//                   \/\  ")
+        print("\|   XXXX     XXXX   | /   ")
+        print(" |   XXXX     XXXX   |/     ")
+        print(" |   XXX       XXX   |      ")
+        print(" |                   |      ")
+        print(" \__      XXX      __/     ")
+        print("   |\     XXX     /|       ")
+        print("   | |           | |        ")
+        print("   | I I I I I I I |        ")
+        print("   |  I I I I I I  |        ")
+        print("   \_             _/       ")
+        print("     \_         _/         ")
+        print("       \_______/           ")
+
+def carrega_palavras():
+    # Lê de um arquivo as palavras para o jogo
+    arquivo=open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
+    return (palavras)
+
+def avalia_chute(chute,letra_certa,palavra_secreta):
+    posicao = 0
+    for letra in palavra_secreta:
+        if (chute == letra):  # letra.upper, tranforma udo em caps lock
+            letra_certa[posicao] = letra
+        posicao = posicao + 1
+def desenha_forca(erros):
+        print("  _______     ")
+        print(" |/      |    ")
+
+        if (erros == 1):
+            print(" |      (_)   ")
+            print(" |            ")
+            print(" |            ")
+            print(" |            ")
+
+        if (erros == 2):
+            print(" |      (_)   ")
+            print(" |      \     ")
+            print(" |            ")
+            print(" |            ")
+
+        if (erros == 3):
+            print(" |      (_)   ")
+            print(" |      \|    ")
+            print(" |            ")
+            print(" |            ")
+
+        if (erros == 4):
+            print(" |      (_)   ")
+            print(" |      \|/   ")
+            print(" |            ")
+            print(" |            ")
+
+        if (erros == 5):
+            print(" |      (_)   ")
+            print(" |      \|/   ")
+            print(" |       |    ")
+            print(" |            ")
+
+        if (erros == 6):
+            print(" |      (_)   ")
+            print(" |      \|/   ")
+            print(" |       |    ")
+            print(" |      /     ")
+
+        if (erros == 7):
+            print(" |      (_)   ")
+            print(" |      \|/   ")
+            print(" |       |    ")
+            print(" |      / \   ")
+
+        print(" |            ")
+        print("_|___         ")
+        print()
+
 '''
         IMPORTANTE
     Chamada recursiva do módulo, esse if é o que permite a execução do modulo individualmente pelo interpretador
@@ -60,8 +163,5 @@ python, uma vez que você chama um modulo ele é guardado na variável __name__,
 executado somente se GameSelector, com essa chamada do modulo para sua propria função podemos portanto executá-lo
 separadamente da função GameSelector.
 '''
-def carrega_palavras():
-    # Lê de um arquivo as palavras para o jogo
-    with open("palavras.txt", "r") as arquivo
 if(__name__=="__main__"):
     jogar_forca()
